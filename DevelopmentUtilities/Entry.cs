baa0293ce36cc8d29cd38ea9039ba6e91d77581f
Tomasz Cichoń <mrciastexx@gmail.com>
2016-02-21 11:21:37 +0100

    Added Development Utilities plugin.

---
+++ DevelopmentUtilities/Entry.cs
@@ -0,0 +1,60 @@
+﻿using DevelopmentUtilities.Utilities;
+using Spectrum.API.Configuration;
+using Spectrum.API.FileSystem;
+using Spectrum.API.Interfaces.Plugins;
+using Spectrum.API.Interfaces.Systems;
+
+namespace DevelopmentUtilities
+{
+    public class Entry : IPlugin
+    {
+        public string FriendlyName => "Development Utilities";
+        public string Author => "Ciastex";
+        public string Contact => "ciastexx@live.com";
+        public int CompatibleAPILevel => 1;
+
+        private PluginData PluginData { get; set; }
+        private Settings Settings { get; set; }
+
+        private SceneDumper SceneDumper { get; set; }
+
+        public void Initialize(IManager manager)
+        {
+            Settings = new Settings(typeof(Entry));
+            ValidateSettings();
+
+            PluginData = new PluginData(typeof(Entry));
+
+            SceneDumper = new SceneDumper(PluginData);
+
+            manager.Hotkeys.Bind(Settings["SceneDumperBriefHotkey"], () =>
+            {
+                SceneDumper.DumpCurrentScene(false);
+            });
+
+            manager.Hotkeys.Bind(Settings["SceneDumperDetailedHotkey"], () =>
+            {
+                SceneDumper.DumpCurrentScene(true);
+            });
+        }
+
+        public void Shutdown()
+        {
+
+        }
+
+        private void ValidateSettings()
+        {
+            if (Settings["SceneDumperBriefHotkey"] == string.Empty)
+            {
+                Settings["SceneDumperBriefHotkey"] = "LeftControl+Alpha0";
+            }
+
+            if (Settings["SceneDumperDetailedHotkey"] == string.Empty)
+            {
+                Settings["SceneDumperDetailedHotkey"] = "LeftControl+Alpha9";
+            }
+            Settings.Save();
+        }
+    }
+}
---
+++ DevelopmentUtilities/Utilities/SceneDumper.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Spectrum.API.FileSystem;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Object = UnityEngine.Object;
+
+namespace DevelopmentUtilities.Utilities
+{
+    internal class SceneDumper
+    {
+        private PluginData PluginData { get; }
+
+        internal SceneDumper(PluginData pluginData)
+        {
+            PluginData = pluginData;
+        }
+
+        public void DumpCurrentScene(bool detailed)
+        {
+            var currentSceneName = SceneManager.GetActiveScene().name;
+            var gameObjects = Object.FindObjectsOfType<GameObject>();
+
+            var validGameObjects = new List<GameObject>();
+            foreach (var gameObject in gameObjects)
+            {
+                if (gameObject.transform.parent == null)
+                    validGameObjects.Add(gameObject);
+            }
+
+            using (var sw = new StreamWriter(PluginData.CreateFile(currentSceneName)))
+            {
+                foreach (var gameObject in validGameObjects)
+                {
+                    DumpGameObject(gameObject, sw, "", detailed);
+                }
+            }
+        }
+
+        private void DumpGameObject(GameObject gameObject, StreamWriter writer, string indent, bool detailed)
+        {
+            writer.WriteLine($"{indent}+{gameObject.name}");
+
+            foreach (var component in gameObject.GetComponents<Component>())
+            {
+                DumpComponent(component, writer, indent + "  ", detailed);
+            }
+
+            foreach (Transform child in gameObject.transform)
+            {
+                DumpGameObject(child.gameObject, writer, indent + "  ", detailed);
+            }
+        }
+
+        private void DumpComponent(Component component, StreamWriter writer, string indent, bool detailed)
+        {
+            writer.WriteLine($"{indent}{component?.GetType().Name ?? "(null)"}");
+
+            if(detailed)
+                DumpFields(component, writer, indent + "  ");
+        }
+
+        private void DumpFields(Component component, StreamWriter writer, string indent)
+        {
+            var publicFields = component.GetType().GetFields();
+            var nonPublicFields = component.GetType().GetFields(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            foreach (var field in nonPublicFields)
+            {
+                writer.WriteLine($"{indent}{field.Name} = {field.GetValue(component)}");
+            }
+
+            foreach (var field in publicFields)
+            {
+                writer.WriteLine($"{indent}{field.Name} = {field.GetValue(component)}");
+            }
+        }
+    }
+}
